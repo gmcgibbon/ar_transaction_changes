626435556633ea44f8d59674348cb803fbe6c744
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2020-03-09 17:59:46 -0400

    Use changes_applied override to hook into store changes

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -18,19 +18,9 @@ module ArTransactionChanges
     @transaction_changed_attributes ||= HashWithIndifferentAccess.new
   end
 
-  method_name = if ActiveRecord.gem_version >= Gem::Version.new("5.2.0.beta1")
-    "_write_attribute"
-  else
-    "write_attribute"
-  end
-
-  define_method(method_name) do |attr_name, value|
-    attr_name = attr_name.to_s
-    old_value = read_attribute(attr_name)
-    ret = super(attr_name, value)
-    unless transaction_changed_attributes.key?(attr_name) || value == old_value
-      transaction_changed_attributes[attr_name] = old_value
-    end
-    ret
+  def changes_applied
+    super
+    changes = saved_changes.transform_values(&:first)
+    transaction_changed_attributes.merge!(changes)
   end
 end
